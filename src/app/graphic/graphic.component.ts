560c568b0ede4b1b3376436dc8e5ae9ecaa8be2c
BrenoSouza <jose.souza@ccc.ufcg.edu.br>
2018-03-18 20:04:46 -0300

    Chart configs

--- src/app/graphic/graphic.component.ts
+++ src/app/graphic/graphic.component.ts
@@ -6,26 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./graphic.component.css']
 })
 export class GraphicComponent implements OnInit {
-  protected lineChartType  = 'line'; 
-  protected lineChartLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  protected lineChartType  = 'line';
+  protected lineChartLabels = ['Nível'];
+  protected lineChartData = [
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+
+  protected lineChartOptions = {
+    responsive: true
+  };
+
+  protected lineChartColors = [
+    {
+      backgroundColor: '#89C4F4',
+      borderColor: '#22A7F0',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    }
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  // lineChart
-  public lineChartData:Array<any> = [
-    [],
-    [28, 48, 40, 19, 86, 27, 90]
-  ];
-
-
-  public chartClicked(e:any):void {
+  chartClicked(e: any) {
     console.log(e);
   }
- 
-  public chartHovered(e:any):void {
+
+  chartHovered(e: any) {
     console.log(e);
   }
 
