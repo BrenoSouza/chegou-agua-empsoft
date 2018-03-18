b77e94f3c8a6f3d69d8040243193230c6ad05f05
BrenoSouza <jose.souza@ccc.ufcg.edu.br>
2018-03-18 17:11:47 -0300

    Deploy

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '/dist')));
 app.use('/', router);
 
 app.get('*', (req, res) => {
